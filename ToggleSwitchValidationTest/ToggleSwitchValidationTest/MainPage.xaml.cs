89b6af232e9d42cdf3b705622de703d05a3e3a10
David <15479248+davepruitt@users.noreply.github.com>
2023-04-25 16:40:17 -0600

    Initial commit

---
+++ ToggleSwitchValidationTest/ToggleSwitchValidationTest/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿namespace ToggleSwitchValidationTest;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+		this.BindingContext = new MainPageViewModel();
+	}
+
+    private async void MySwitch_Toggled(object sender, ToggledEventArgs e)
+    {
+		var result = await DisplayAlert("Are you sure?", string.Empty, "Yes", "No");
+		var vm = this.BindingContext as MainPageViewModel;
+		if (vm != null)
+		{
+			if (result)
+			{
+				vm.IsMySwitchToggled = e.Value;
+			}
+			else
+			{
+				vm.NotifyPropertyChanged("IsMySwitchToggled");
+			}
+		}
+    }
+}
+
---
+++ ToggleSwitchValidationTest/ToggleSwitchValidationTest/MainPageViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToggleSwitchValidationTest
+{
+    public class MainPageViewModel : INotifyPropertyChanged
+    {
+        private bool _is_my_switch_toggled = false;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public MainPageViewModel()
+        {
+            //empty
+        }
+
+        public bool IsMySwitchToggled
+        {
+            get
+            {
+                return _is_my_switch_toggled;
+            }
+            set
+            {
+                _is_my_switch_toggled = value;
+                NotifyPropertyChanged("IsMySwitchToggled");
+            }
+        }
+
+        public void NotifyPropertyChanged (string propertyName)
+        {
+            PropertyChanged?.Invoke (this, new PropertyChangedEventArgs (propertyName));
+        }
+    }
+}
